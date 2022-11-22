1cf1f36561975e8b473941b6be59abbba6b1e91b
ArthurDelignette <84783806+ArthurDelignette@users.noreply.github.com>
2022-11-22 22:44:02 +0100

    Changement d'orga des TP pour avoir tout dans un même projet juste des activités différentes et mise a jour graphique

--- tp1/app/src/main/java/com/example/tp1/MainActivity.java
+++ tp1/app/src/main/java/com/example/tp1/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.tp1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,70 +10,16 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView log;
-    private int numero;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Bundle bundle = getIntent().getExtras();
-        if(bundle != null){
-            numero = bundle.getInt("numero");
-            numero++;
-        }
-
-        log = findViewById(R.id.log);
-        log.append("onCreate " + numero + "\n");
-
-        Button start = findViewById(R.id.start);
-        start.setOnClickListener(view -> {
-            Intent intent = new Intent(MainActivity.this, MainActivity.class);
-            intent.putExtra("numero", numero);
+        AppCompatButton appCompatButton = findViewById(R.id.button_TP1);
+        appCompatButton.setOnClickListener(view -> {
+            Intent intent = new Intent(this, TP1_Activity.class);
             startActivity(intent);
         });
 
-        Button finish = findViewById(R.id.finish);
-        finish.setOnClickListener(view -> {
-            finish();
-        });
-
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        log.append("onStart\n");
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        log.append("onRestart\n");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        log.append("onStop\n");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        log.append("onDestroy\n");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        log.append("onPause\n");
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        log.append("onResume\n");
-    }
 }
