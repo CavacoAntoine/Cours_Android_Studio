3ae1eb79eecba2dcd504bfb8cc2331e34eacded5
Antoine Cavaco <antoinecavaco@gmail.com>
2022-11-29 17:22:14 +0100

    tp1 correction

--- tp1/app/src/main/java/com/example/tp1/MainActivity.java
+++ tp1/app/src/main/java/com/example/tp1/MainActivity.java
@@ -5,21 +5,75 @@ import androidx.appcompat.widget.AppCompatButton;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView log;
+    private int numero;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        AppCompatButton appCompatButton = findViewById(R.id.button_TP1);
-        appCompatButton.setOnClickListener(view -> {
-            Intent intent = new Intent(this, TP1_Activity.class);
+
+        Bundle bundle = getIntent().getExtras();
+        if(bundle != null){
+            numero = bundle.getInt("numero");
+            numero++;
+        }
+
+        log = findViewById(R.id.log);
+        log.append("onCreate " + numero + "\n");
+
+        AppCompatButton start = findViewById(R.id.start);
+        start.setOnClickListener(view -> {
+            Intent intent = new Intent(this, MainActivity.class);
+            intent.putExtra("numero", numero);
             startActivity(intent);
         });
 
+        AppCompatButton finish = findViewById(R.id.finish);
+        finish.setOnClickListener(view -> {
+            finish();
+        });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        log.append("onStart\n");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        log.append("onRestart\n");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        log.append("onStop\n");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        log.append("onDestroy\n");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        log.append("onPause\n");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        log.append("onResume\n");
     }
 
 }
