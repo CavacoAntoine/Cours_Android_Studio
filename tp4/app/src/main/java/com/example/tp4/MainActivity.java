ad722c99ef9639424dcbca9b0c78057d7c9cf4a9
Arthur Delignette <arthur@dynseo.com>
2022-12-13 14:00:10 +0100

    tp4 : layout lie a l'activité

--- tp4/app/src/main/java/com/example/tp4/MainActivity.java
+++ tp4/app/src/main/java/com/example/tp4/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.tp4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button start,stop;
+    private TextView output;
+    private ProgressBar progress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.start = findViewById(R.id.buttonStart);
+        this.stop = findViewById(R.id.buttonStop);
+        this.output = findViewById(R.id.text);
+        this.progress = findViewById(R.id.progress);
+
+        this.start.setOnClickListener(view ->
+        {
+            
+        });
     }
 }
