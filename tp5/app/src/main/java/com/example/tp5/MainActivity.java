70022fc82bed91602051088778bcd146bc9b005d
Antoine Cavaco <antoinecavaco@gmail.com>
2022-12-16 13:17:16 +0100

    button name

--- tp5/app/src/main/java/com/example/tp5/MainActivity.java
+++ tp5/app/src/main/java/com/example/tp5/MainActivity.java
@@ -3,8 +3,13 @@ package com.example.tp5;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText ip, port, x, y;
+    private Button set, next, previous, up, down, tab, click, clickDouble, move, beep, mousePress, mouseRelease, getScreenSize, exit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
