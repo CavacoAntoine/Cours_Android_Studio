2927dd7eef9e09006b8a4d1ec25efffefa92acf0
Antoine Cavaco <antoinecavaco@gmail.com>
2022-12-16 14:30:24 +0100

    button name

--- tp5/app/src/main/java/com/example/tp5/MainActivity.java
+++ tp5/app/src/main/java/com/example/tp5/MainActivity.java
@@ -4,22 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.MotionEvent;
-import android.view.View;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 
-import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
-import java.net.UnknownHostException;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.SynchronousQueue;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText ip, port, x, y;
     private Button set, next, previous, up, down, tab, click, move, beep;
     private long lastClick, currentClick;
+    private DataOutputStream out;
+    private Socket socket;
+    private Sender sender;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,16 +33,15 @@ public class MainActivity extends AppCompatActivity {
         this.set.setOnClickListener(view -> this.onClickAction(new String[]{"set",
                 this.ip.getText().toString(), this.port.getText().toString()}));
 
-        this.next.setOnClickListener(view -> this.onClickAction(new String[]{"next"}));
-        this.previous.setOnClickListener(view -> this.onClickAction(new String[]{"previous"}));
-        this.up.setOnClickListener(view -> this.onClickAction(new String[]{"up"}));
-        this.down.setOnClickListener(view -> this.onClickAction(new String[]{"down"}));
-        this.tab.setOnClickListener(view -> this.onClickAction(new String[]{"tab"}));
+        //this.next.setOnClickListener(view -> this.onClickAction(new String[]{"next"}));
+        //this.previous.setOnClickListener(view -> this.onClickAction(new String[]{"previous"}));
+        //this.up.setOnClickListener(view -> this.onClickAction(new String[]{"up"}));
+        //this.down.setOnClickListener(view -> this.onClickAction(new String[]{"down"}));
+        //this.tab.setOnClickListener(view -> this.onClickAction(new String[]{"tab"}));
+        this.beep = this.findViewById(R.id.buttonBeep);
+        this.beep.setOnClickListener(view -> this.onClickAction(new String[]{"beep"}));
+        this.click = this.findViewById(R.id.buttonClick);
         this.click.setOnClickListener(view -> this.onClickAction(new String[]{"click"}));
-        this.click.setOnLongClickListener(view -> {
-            this.onClickAction(new String[]{"press"});
-            return true;
-        });
     }
 
     private void onClickAction(String[] strings){
@@ -66,38 +67,55 @@ public class MainActivity extends AppCompatActivity {
                     this.currentClick = 0;
                     //Double click
                 }
-
-                //one click
-                break;
-            case "press":
-                break;
-            case "release":
+                this.sender.offer("click".getBytes());
                 break;
             case "move":
                 break;
             case "beep":
+                this.sender.offer("beep".getBytes());
                 break;
             default:
                 return ;
         }
     }
 
-    private class TCPAction extends AsyncTask<String, Void, String> {
+    public class Sender extends Thread {
+        private BlockingQueue<byte[]> queue;
+
+        public Sender() {
+            queue = new SynchronousQueue<byte[]>();
+            this.start();
+        }
+
+        public boolean offer(byte[] cmd) {
+            return queue.offer(cmd);
+        }
+
+        public void close() {
+            this.interrupt();
+        }
+
+        public void run() {
+            while (!isInterrupted()) {
+                try {
+                    byte[] cmd = queue.take();
+                    out.write(cmd);
+                } catch (Exception e) {
+                }
+            }
+        }
+
+    }
+
+    private class SocketInitalize extends AsyncTask<String, Void, Void> {
 
         @Override
-        protected String doInBackground(String... strings) {
-            while(!isCancelled()){
-                try(Socket socket = new Socket("10.0.0.2.2", 80)) {
-                    DataOutputStream out= new DataOutputStream( socket.getOutputStream());
-
-                    out.write(strings[0].getBytes());
-                    out.write("\n".getBytes());
-                    return null;
-                } catch (IOException e) {
-                    e.printStackTrace();
-                    return null;
-                }
-
+        protected Void doInBackground(String... strings) {
+            try {
+                socket = new Socket(strings[0], Integer.parseInt(strings[1]));
+                out = new DataOutputStream(socket.getOutputStream());
+            } catch (IOException e) {
+                Log.e("Socket", e.getMessage());
             }
             return null;
         }
