63922c1bcf2aef8fbbb566e0b2dd947d5174f352
Antoine Cavaco <antoinecavaco@gmail.com>
2023-01-10 15:10:38 +0100

    TP6

---
+++ tp6/app/src/main/java/com/example/tp6/courseItemAdapter.java
@@ -0,0 +1,42 @@
+package com.example.tp6;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import java.util.List;
+
+public class courseItemAdapter extends BaseAdapter {
+
+    private Context context;
+    private List<courseItem> items;
+
+    public courseItemAdapter(Context context, List<courseItem> items) {
+        this.context = context;
+        this.items = items;
+    }
+
+    @Override
+    public int getCount() {
+        return this.items.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return this.items.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+        View itemView = inflater.inflate(R.layout.item, viewGroup, false);
+        return null;
+    }
+}
