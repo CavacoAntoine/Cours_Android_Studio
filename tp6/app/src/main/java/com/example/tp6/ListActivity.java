892685418a7481ba367b2a1bb9d30a7e6bdb5f26
Antoine Cavaco <antoinecavaco@gmail.com>
2023-01-10 16:08:15 +0100

    TP6

--- tp6/app/src/main/java/com/example/tp6/ListActivity.java
+++ tp6/app/src/main/java/com/example/tp6/ListActivity.java
@@ -11,7 +11,7 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class listActivity extends AppCompatActivity {
+public class ListActivity extends AppCompatActivity {
 
     private TextView title;
     private ListView listView;
