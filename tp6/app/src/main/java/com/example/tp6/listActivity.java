6cc8b0d81064389246bbb4cb7fd8240f55109eae
Arthur Delignette <arthur@dynseo.com>
2023-01-10 16:05:01 +0100

    oui9

--- tp6/app/src/main/java/com/example/tp6/listActivity.java
+++ tp6/app/src/main/java/com/example/tp6/listActivity.java
@@ -1,14 +1,32 @@
 package com.example.tp6;
 
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class listActivity extends AppCompatActivity {
+
+    private TextView title;
+    private ListView listView;
+    private Button add;
+    private EditText nom, quantity;
+    private ImageView micro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.list);
+        this.title = findViewById(R.id.textList);
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            this.title.setText(extras.getString("nom"));
+        }
     }
 }
