6a49aa7c3c9aaddfb049b24123c559dc805a1a96
Antoine Cavaco <antoinecavaco@gmail.com>
2023-01-13 10:19:12 +0100

    TP6

--- tp6/app/src/main/java/com/example/tp6/MainActivity.java
+++ tp6/app/src/main/java/com/example/tp6/MainActivity.java
@@ -8,19 +8,17 @@ import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText editText;
-    private ArrayList<String> listes;
-    private ArrayAdapter<String> stringAdapter;
+    private EditText editText, editLat, editLong;
+    private ArrayList<ListItem> listes;
+    private ListItemAdapter listItemAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,25 +33,27 @@ public class MainActivity extends AppCompatActivity {
         addButton.setOnClickListener(this::clickAjouter);
 
         ListView listView = findViewById(R.id.list);
-        this.stringAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, this.listes);
-        listView.setAdapter(this.stringAdapter);
+        this.listItemAdapter = new ListItemAdapter(this, this.listes);
+        listView.setAdapter(this.listItemAdapter);
         listView.setOnItemLongClickListener(this::longItemClick);
         listView.setOnItemClickListener(this::itemClick);
 
         this.editText = findViewById(R.id.editText);
+        this.editLat = findViewById(R.id.editLat);
+        this.editLong = findViewById(R.id.editLong);
     }
 
     private void itemClick(AdapterView<?> adapterView, View view, int i, long l) {
         this.saveList();
         Intent intent = new Intent(this, ListActivity.class);
-        intent.putExtra("nom", this.listes.get(i));
+        intent.putExtra("nom", this.listes.get(i).getNom());
         startActivity(intent);
     }
 
     private void saveList() {
         StringBuilder sharedListe = new StringBuilder();
-        for( String liste : this.listes) {
-            sharedListe.append(liste).append(",");
+        for( ListItem liste : this.listes) {
+            sharedListe.append(liste.getNom()).append(",").append(liste.getLatitude()).append(",").append(liste.getLongitude()).append(",");
         }
 
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
@@ -68,32 +68,37 @@ public class MainActivity extends AppCompatActivity {
         if(sharedListes == null)
             return false;
 
-        String[] noms = sharedListes.split(",");
+        String[] listes = sharedListes.split(",");
 
-        this.listes = new ArrayList<>();
-        this.listes.addAll(Arrays.asList(noms));
+        for(int i = 0; i < listes.length-2; i++ ) {
+            this.listes.add(new ListItem(listes[i], Double.parseDouble(listes[i+1]), Double.parseDouble(listes[i+2])));
+        }
         return true;
     }
 
     private void clickAjouter(View view) {
         String nom = this.editText.getText().toString();
+        double latitude = Double.parseDouble(this.editLat.getText().toString()) ;
+        double longitude = Double.parseDouble(this.editLong.getText().toString());
         if(nom.isEmpty())
             return;
-        this.listes.add(nom);
-        this.stringAdapter.notifyDataSetChanged();
+        this.listes.add(new ListItem(nom, latitude, longitude));
+        this.listItemAdapter.notifyDataSetChanged();
         this.editText.setText("");
+        this.editLat.setText("");
+        this.editLong.setText("");
     }
 
     private boolean longItemClick(AdapterView<?> adapterView, View view, int position, long id) {
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-        String sharedListe = prefs.getString(this.listes.get(position), null);
+        String sharedListe = prefs.getString(this.listes.get(position).getNom(), null);
         if(sharedListe != null) {
             SharedPreferences.Editor edit = prefs.edit();
-            edit.remove(this.listes.get(position));
+            edit.remove(this.listes.get(position).getNom() + "," + this.listes.get(position).getLatitude() + "," + this.listes.get(position).getLongitude()+",");
             edit.apply();
         }
         this.listes.remove(position);
-        this.stringAdapter.notifyDataSetChanged();
+        this.listItemAdapter.notifyDataSetChanged();
         this.saveList();
         return true;
     }
